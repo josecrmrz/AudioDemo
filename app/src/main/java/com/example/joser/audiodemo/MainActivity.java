954c400758c7f9c5869da269482aa5b29f15f533
Jose Ramirez <josecrmrz@gmail.com>
2018-04-16 22:12:49 -0500

    Fix stuttering sound

--- app/src/main/java/com/example/joser/audiodemo/MainActivity.java
+++ app/src/main/java/com/example/joser/audiodemo/MainActivity.java
@@ -2,9 +2,8 @@ package com.example.joser.audiodemo;
 
 import android.media.AudioManager;
 import android.media.MediaPlayer;
-import android.provider.MediaStore;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.SeekBar;
 
@@ -15,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
 
     MediaPlayer mediaPlayer = null;
     AudioManager audioManager = null;
+    Timer timer = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,23 +61,34 @@ public class MainActivity extends AppCompatActivity {
         scrubSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                mediaPlayer.seekTo(progress);
+                if (fromUser) {
+                    mediaPlayer.seekTo(progress);
+                }
             }
 
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
                 mediaPlayer.pause();
+                timer.cancel();
             }
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
                 mediaPlayer.start();
+                timer = new Timer();
+                timer.scheduleAtFixedRate(new TimerTask() {
+                    @Override
+                    public void run() {
+                        scrubSeekBar.setProgress(mediaPlayer.getCurrentPosition());
+                    }
+                }, 0, 300);
             }
         });
 
         // No lag if this is removed, but no seek bar animation
         // decrease the period will make it more noticeable
-        new Timer().scheduleAtFixedRate(new TimerTask() {
+        timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
             @Override
             public void run() {
                 scrubSeekBar.setProgress(mediaPlayer.getCurrentPosition());
